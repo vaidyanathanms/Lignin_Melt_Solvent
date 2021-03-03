31747d3e924792e7c501d22aa725bb51d41297e5
vaidyanathanms <msvaidyanathan1729@gmail.com>
2021-03-03 16:47:31 -0500

    added all thermostat coefficients. to work on mdp/sh files

--- files_ligmelt_solv/myruns/src_py/input_gen.py
+++ files_ligmelt_solv/myruns/src_py/input_gen.py
@@ -14,10 +14,17 @@ import math
 from make_gmx_inputs import * # function definitions
 #------------------------------------------------------------------
 
-# Version Info and number of input arguments
+# Version Info and # of input args for parsing thermostat coeff
 print("Generating GROMACS run-time inputs")
 print("Version: Mar-03-2021")
-print 
+if len(sys.argv) == 1:
+    coeff_fyle = 'None' 
+elif len(sys.argv) == 2:
+    coeff_fyle = str(sys.argv[1])
+else:
+    print('Unknown number of arguments: ', len(sys.argv),\
+          str(sys.argv))
+    exit()
 #------------------------------------------------------------------
 
 # Input Data
@@ -70,9 +77,9 @@ for casenum in range(num_cases):
     if not os.path.isdir(workdir1):
         os.mkdir(workdir1)
 
-    # Set thermostat coefficients
-    temp_vel_resc,temp_par_rah,pres_par_rah = couple_coeff(coeff_fyle,
-    \)
+    # Set thermostat variables (change if there is a temp sweep)
+    temp_nvt,temp_npt_berend,temp_npt_parrah,pres_berend,pres_parrah,\
+        ref_temp, ref_pres = couple_coeff(inp_type,coeff_fyle)
 
     # Change to working directory
     os.chdir(workdir1)
@@ -84,93 +91,5 @@ for casenum in range(num_cases):
     check_cpy_mdp_files(mdp_dir,workdir1,sh_fyles)
     rerun = cpy_edit_sh_files(sh_dir,workdir1,mdp_fyles)
 
-
+    
         
-    for iarch in range(len(archarr)):
-             
-        if archarr[iarch] == 1:
-            print( "Archval: Block_Block")
-            dirstr = 'bl_bl'
-            fylstr = 'block_block'
-        elif archarr[iarch] == 2:
-            print( "Archval: Block_Alter")
-            dirstr = 'bl_al'
-            fylstr = 'block_alter'
-        elif archarr[iarch] == 3:
-            print( "Archval: Alter_Block")
-            dirstr = 'al_bl'
-            fylstr = 'alter_block'
-        elif archarr[iarch] == 4:
-            print( "Archval: Alter_Alter")
-            dirstr = 'al_al'
-            fylstr = 'alter_alter'
-        else:
-            print( "Unknown Architecture")
-            
-        workdir_arch = workdir2 + '/' + dirstr
-
-        if not os.path.isdir(workdir_arch):
-            print("ERROR: ", workdir_arch, " not found")
-            continue
-
-        workdir_freepdi = workdir_arch + '/pdi_free_' + str(pdi_free)
-        if not os.path.isdir(workdir_freepdi):
-            print("ERROR: ", workdir_freepdi, " not found")
-            continue
-
-        workdir_graftpdi = workdir_freepdi + '/pdi_graft_' + str(pdi_graft)
-        if not os.path.isdir(workdir_graftpdi):
-            print("ERROR: ", workdir_graftpdi, " not found")
-            continue
-
-        for casenum in range(len(ncases_pdi)):
-
-            workdir_subpdi = workdir_graftpdi + '/Case_' + str(ncases_pdi[casenum])
-            if not os.path.isdir(workdir_subpdi):
-                print("ERROR: ", workdir_subpdi, " not found")
-                continue
-
-            os.chdir(workdir_subpdi)
-            destdir = os.getcwd()
-
-            print( "Starting analysis for case ", ncases_pdi[casenum], "in ",\
-                       free_chains[ifree],dirstr)
-
-            #---Copying files------
-            print( "Current Dir ", destdir)
-            print( "Copying Files")
-                
-            for fyllist in range(len(ana_files)):
-                cpy_main_files(maindir,destdir,ana_files[fyllist])
-
-            for fyllist in range(len(job_files)):
-                cpy_main_files(jobdir,destdir,job_files[fyllist])
-
-            dataname =  find_datafyle(free_chains[ifree],fylstr,\
-                                      ncases_pdi[casenum],destdir)
-            if dataname == 'ERROR':
-                print("ERROR: No restart files found")
-                continue
-
-            
-            #latest_traj = find_latest_trajfyle(traj_pref,destdir)
-            #if latest_traj == 'ERROR':
-            #    print("ERROR: No trajectory files found")
-            #    continue
-            
-            compile_anafiles()            
-            ntotch = free_chains[ifree] + graft_chains
-            traj_arr = glob.glob(traj_pref)
-            if traj_arr == []:
-                print("ERROR: No trajectory files found")
-                continue
-            
-            for fyllist in range(len(traj_arr)):
-                print("Analyzing ", traj_arr[fyllist])
-                edit_generate_anainp_files(dataname,traj_arr[fyllist],ntotch,\
-                                           free_chains[ifree],graft_chains,\
-                                           cutoff_dist,fyllist+1)
-                outana = 'jobana_' + str(fyllist+1) + '.sh'
-                run_analysis(free_chains[ifree],pdi_free,ncases_pdi[casenum],\
-                             fylstr,'jobana_var.sh',outana,fyllist+1)
-
--- files_ligmelt_solv/myruns/src_py/make_gmx_inputs.py
+++ files_ligmelt_solv/myruns/src_py/make_gmx_inputs.py
@@ -26,12 +26,44 @@ def gencpy(dum_maindir,dum_destdir,fylname):
 #------------------------------------------------------------------
 
 # Set default thermostat coefficients
-def couple_coeff(coeff_fyle = 'None'):
-    temp_velresc = 0.1
-    temp_parrah  = 0.3
-    pres_parrah  = 8.0
-    if coeff_fyle = 'None':
-        
+def couple_coeff(inp_type,coeff_fyle = 'None'):
+    # default. change if needed
+    temp_nvt = 0.1
+    temp_npt_berend  = 0.1
+    temp_npt_parrah  = 0.2
+    pres_berend = 0.5
+    ref_temp = 300
+    ref_pres = 1
+    if inp_type == 'melts':
+        pres_parrah  = 5.0
+    else:
+        pres_parrah  = 8.0
+    if coeff_fyle != 'None':
+        with open(coeff_fyle) as farg:
+            for line in farg:
+                line = line.rstrip('\n')
+                if line.startswith('#'):
+                    continue
+                words = line.split()
+                if words[0] = 'Temp_NVT':
+                    temp_nvt  = 0.1
+                elif words[0] == 'Temp_NPT_Berendsen':
+                    temp_npt_berend = float(words[1])
+                elif words[0] == 'Temp_NPT_Parrinello':
+                    temp_npt_parrah = float(words[1])
+                elif words[0] == 'Pres_Berendsen':
+                    temp_npt_parrah = float(words[1])
+                elif words[0] == 'Pres_Parrinello':
+                    pres_parrah = float(words[1])
+                elif words[0] == 'Ref_Temp':
+                    ref_temp  = float(words[1])
+                elif words[0] == 'Ref_Pres':
+                    ref_pres  = float(words[1])
+                else:
+                    raise RuntimeError("Unknown keyword: "+ words[0] \
+                                       + "in" + str(coeff_fyle))
+    return temp_vres,temp_parrah,pres_parrah
+#------------------------------------------------------------------
 
 #Check pdb/psf/top files
 def check_inp_files(dum_inpdir):
@@ -45,8 +77,9 @@ def check_inp_files(dum_inpdir):
 
 # Check for mdp files and copy/edit if not present
 def check_cpy_mdp_files(srcdir,destdir,mdp_fyles):
+    # 
 
-
+#------------------------------------------------------------------
 # Copy and edit mdp files
 def cpy_edit_sh_files(srcdir,destdir,sh_fyles):
     # Check for tpr files to provide run conditions
@@ -66,10 +99,7 @@ def cpy_edit_sh_files(srcdir,destdir,sh_fyles):
             
 
 
-#Directory Paths
 
-
-#Required Files
 
 
 #Main Code
