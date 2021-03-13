d16166595b6b9b5ada851523a288c3e91ff0a975
vaidyanathanms <msvaidyanathan1729@gmail.com>
2021-03-13 17:03:03 -0500

    updated up to making shell script changes

--- files_ligmelt_solv/myruns/src_py/input_gen.py
+++ files_ligmelt_solv/myruns/src_py/input_gen.py
@@ -28,37 +28,44 @@ else:
 #------------------------------------------------------------------
 
 # Input Data
-inp_type  = 'melts' # melts, solvents, cosolvents
-biomass   = 'switchgrass' # name of the biomass type
-disperse  = 'mono'  # mono/poly; only for melts
-num_cases = 5 # for solvents, this will be == len(norg_solv)
-nchains   = 50 # number of chains in the polymer 
+inp_type  = 'cosolvents'       # melts, solvents, cosolvents
+biomass   = 'WT' # name of the biomass type
+disperse  = 'mono' # mono/poly; only for melts
+o_sol_typ = 'EOH'  # relevant only for solvents/cosolvents
+num_cases = 3      # for solvents, this will be == len(norg_solv)
+nchains   = 1     # number of polymer chains
 norg_solv = [1000] # number of organic solvents
 nwater    = [4000] # number of water molecules (for cosolvents)
 #------------------------------------------------------------------
 
 # Directory Paths
-main_dir = os.getcwd()
-gmx_dir  = '../src_gmx'
-mdp_dir  = gmx_dir + '/' + 'mdp_files'
-sh_dir   = gmx_dir + '/' + 'sh_files'
-scr_dir  = '/gpfs/alpine/bip189/scratch/vaidyams/'
+main_dir  = os.getcwd()
+gmx_dir   = '../src_gmx'
+top_dir   = gmx_dir + '/solv_files/topol'
+conf_dir  = gmx_dir + '/solv_files/initguess'
+prm_dir   = gmx_dir + '/solv_files/prm_itp'
+mdp_dir   = gmx_dir + '/' + 'mdp_files'
+sh_dir    = gmx_dir + '/' + 'sh_files'
+scr_dir   = '/gpfs/alpine/bip189/scratch/vaidyams/'
 if not os.path.isdir(scr_dir):
     print("FATAL ERROR: ", scr_dir, " not found")
     exit("Check scratch directory path")
-scr_dir  = scr_dir + '/' + inp_type
-work_dir = '/gpfs/alpine/bip189/scratch/vaidyams/lignin/melts/large_systems'
+scr_dir  = scr_dir + '/lignin'
+if not os.path.isdir(scr_dir):
+    os.mkdir(scr_dir)
 #------------------------------------------------------------------
 
-# Required GMX Files
-mdp_fyles = ['minim_pyinp.mdp','nvt_pyino.mdp',\
-             'npt_berendsen_pyinp.mdp','npt_main_pyinp.mdp']
+# Required GMX/sh Files
+mdp_fyles  = ['minim_pyinp.mdp','nvt_pyino.mdp',\
+              'npt_berendsen_pyinp.mdp','npt_main_pyinp.mdp']
+sh_md_fyle = 'run_md_pyinp.inp'
+sh_pp_fyle = 'run_preprocess_pyinp.sh'
 #------------------------------------------------------------------
 
 #Main Code
 for casenum in range(num_cases):
     
-    print( "Case number: ", casenum + 1)
+    print( "Run number: ", casenum + 1)
       
     # Make directories
     head_dir = scr_dir + '/' + inp_type
@@ -69,17 +76,26 @@ for casenum in range(num_cases):
     if not os.path.isdir(poly_dir):
         os.mkdir(poly_dir)
 
-    disper_dir = poly_dir + '/' + disperse
-    if not os.path.isdir(dispdir):
-        os.mkdir(disper_dir)
+    if inp_type == 'melts':
+        poly_dir = poly_dir + '/' + disperse
+        if not os.path.isdir(poly_dir):
+            os.mkdir(poly_dir)
     
-    workdir1 = disper_dir + '/casenum_' + str(casenum+1)
+    workdir1 = poly_dir + '/run_' + str(casenum+1)
     if not os.path.isdir(workdir1):
-        os.mkdir(workdir1)
+        print('ERR: Create path and input top/pdb files first')
+        continue
 
     # Set thermostat variables (change if there is a temp sweep)
     Tetau_nvt,Tetau_berend,Tetau_parrah,Prtau_berend,Prtau_parrah,\
-        ref_temp, ref_pres = couple_coeff(inp_type,coeff_fyle)
+        ref_temp,ref_pres = couple_coeff(inp_type,coeff_fyle)
+
+    # Copy and edit mdp files
+    check_cpy_mdp_files(mdp_dir,workdir1,mdp_fyles,Tetau_nvt,\
+                        Tetau_berend,Tetau_parrah,Prtau_berend,\
+                        Prtau_parrah,ref_temp,ref_pres,tc_grps,tc_type)
+    cont_run,edit_sh_fyle = cpy_edit_sh_files(sh_dir,workdir1,\
+                                              sh_pp_fyle,sh_md_fyle)
 
     # Change to working directory
     os.chdir(workdir1)
@@ -87,11 +103,17 @@ for casenum in range(num_cases):
     # Check for pdb/psf/top files
     check_inp_files(workdir1)
 
-    # Copy and edit mdp files
-    check_cpy_mdp_files(mdp_dir,workdir1,mdp_fyles,Tetau_nvt,\
-                        Tetau_berend,Tetau_parrah,Prtau_berend,\
-                        Prtau_parrah,ref_temp,ref_pres,tc_grps,tc_type)
-    rerun = cpy_edit_sh_files(sh_dir,workdir1,sh_fyles)
+    # Copy/Edit top/conf/prm files for solvents/cosolvents
+    ff_dir = create_ff_dir(workdir1)
+    if inp_type == 'solvents' or inp_type == 'cosolvents':
+        if cont_run = 0:
+            top_fyl,prm_fyl=cpy_solv_files(top_dir,gro_dir,prm_dir,\
+                                           ff_dir,inp_type,o_sol_typ,\
+                                           wat_typ)
+
+            edit_main_top_file(ff_dir,top_fyl,prm_fyl)
+
+    # Edit shell script files
 
     
         
--- files_ligmelt_solv/myruns/src_py/make_gmx_inputs.py
+++ files_ligmelt_solv/myruns/src_py/make_gmx_inputs.py
@@ -49,10 +49,10 @@ def couple_coeff(inp_type,coeff_fyle = 'None'):
                     tau_temp_nvt  = 0.1
                 elif words[0] == 'TauTemp_Berendsen':
                     tau_temp_berend = float(words[1])
-                elif words[0] == 'TauTemp_Parrinello':
-                    tau_temp_parrah = float(words[1])
                 elif words[0] == 'TauPres_Berendsen':
                     tau_pres_berend = float(words[1])
+                elif words[0] == 'TauTemp_Parrinello':
+                    tau_temp_parrah = float(words[1])
                 elif words[0] == 'TauPres_Parrinello':
                     tau_pres_parrah = float(words[1])
                 elif words[0] == 'Ref_Temp':
@@ -67,62 +67,155 @@ def couple_coeff(inp_type,coeff_fyle = 'None'):
 #------------------------------------------------------------------
 
 #Check pdb/psf/top files
-def check_inp_files(dum_inpdir):
+def check_inp_files(dum_inpdir,top_name = 'None'):
     # check structure files (.pdb/.gro)
     if glob.glob(dum_inpdir+'/*.pdb') == [] and \
        glob.glob(dum_inpdir+'/*.gro') == []:
-        raise RuntimeError("No structure (pdb/gro) files found")
+        raise RuntimeError("No polymer pdb/gro files found")
     if glob.glob(dum_inpdir+'/*.top') == []:
-        raise RuntimeError("No topology (top) files found")
+        raise RuntimeError("No polymer topology files found")
+    if top_name != 'None':
+        if not os.path.exists(dum_inpdir + '/' + top_name):
+            raise RuntimeError("Specified poly top file not found")
+    else len(glob.glob(dum_inpdir+'/*.top')) > 1 and \
+       top_name == 'None':
+        print('More than one topology file found. Using latest')
+           
 #------------------------------------------------------------------
 
 # Check for mdp files and copy/edit if not present
 def check_cpy_mdp_files(srcdir,destdir,mdp_fyles,Tetau_nvt,\
                         Tetau_berend,Tetau_parrah,Prtau_berend,\
                         Prtau_parrah,ref_temp,ref_pres,tc_grpdata,\
-                        tc_grptype):
-     if not os.path.exists(mdp_fyles[fylcnt]):
-         gencpy(srcdir,desdir,mdp_fyles[fylcnt])
-         py_fname = mdp_fyles[fylcnt]
-         rev_fname = py_fname.replace('_pyinp','')
-         print(rev_fname)
-         fr  = open(py_fname,'r')
-         fw  = open(rev_fname,'w')
+                        tc_grptype,coeff_fyle='None'):
 
-         fid = fr.read().replace("py_tcgrps",tc_grpdata).\
-               replace("py_grptype",tc_grptype).\
-               replace("py_Temptau_vr",str(Tetau_nvt)).\
-               replace("py_Temptau_Berend", str(Tetau_berend)).\
-               replace("py_Temptau_ParRah",str(Tetau_parrah)).\
-               replace("py_Prestau_Berend",str(Prtau_berend)).\
-               replace("py_Prestau_ParRah", str(Prtau_parrah)).\
-               replace("py_ref_t",str(ref_temp)).\
-               replace("py_ref_p",str(ref_pres))
-         fw.write(fid)
-         fw.close()
-         fr.close()
+    if coeff_fyle != 'None':
+        gencpy(srcdir,destdir,coeff_fyle)
 
+    if not os.path.exists(mdp_fyles[fylcnt]):
+        gencpy(srcdir,destdir,mdp_fyles[fylcnt])
+        py_fname = mdp_fyles[fylcnt]
+        rev_fname = py_fname.replace('_pyinp','')
+        print(rev_fname)
+        fr  = open(py_fname,'r')
+        fw  = open(rev_fname,'w')
+        fid = fr.read().replace("py_tcgrps",tc_grpdata).\
+              replace("py_grptype",tc_grptype).\
+              replace("py_Temptau_vr",str(Tetau_nvt)).\
+              replace("py_Temptau_Berend", str(Tetau_berend)).\
+              replace("py_Temptau_ParRah",str(Tetau_parrah)).\
+              replace("py_Prestau_Berend",str(Prtau_berend)).\
+              replace("py_Prestau_ParRah", str(Prtau_parrah)).\
+              replace("py_ref_t",str(ref_temp)).\
+              replace("py_ref_p",str(ref_pres))
+        fw.write(fid)
+        fw.close()
+        fr.close()
 #------------------------------------------------------------------
-# Copy and edit mdp files
-def cpy_edit_sh_files(srcdir,destdir,sh_fyles):
+
+# Copy and edit shell script files
+def cpy_edit_sh_files(srcdir,destdir,sh_pp_fyle,sh_md_fyle):
+    edit_sh_fyle = 0
     # Check for tpr files to provide run conditions
     if glob.glob(destdir+'/*.tpr') == []:
         print('No tpr files found. Beginning new runs..')
-        rerun = 0
+        continue_run = 0
+        edit_sh_fyle = 1
     else:
         print('tpr files found. Continuing runs..')
-        rerun = 1
+        continue_run = 1
 
-    if rerun == 1: #tpr files present
-        for fylcnt in range(len(sh_fyles)):
-            if not os.path.exists(sh_fyles[fylcnt]):
-                raise RuntimeError(sh_fyles[fylcnt] + "not found in" \
-                                   + srcdir)
-            gencpy(srcdir,desdir,sh_fyles[fylcnt])
+    if rerun == 0: 
+        if not os.path.exists(srcdir+'/' + sh_pp_fyle):
+            raise RuntimeError(sh_pp_fyle + "not found in" +\
+                               srcdir)
+        gencpy(srcdir,desdir,sh_pp_fyle)
+
+        if not os.path.exists(srcdir+'/' + sh_md_fyle):
+            raise RuntimeError(sh_md_fyle + "not found in" +\
+                               srcdir)
+        gencpy(srcdir,desdir,sh_md_fyle)
+        
+
+    if rerun == 1: #at least one tpr file present
+        sh_mdrun = sh_md_fyle.replace('_pyinp','')
+        if not os.path.exists(destdir+'/' + sh_mdrun):
+            print("WARNING: ", sh_mdrun, "not found in", destdir)
+            print("Recopying ", sh_md_fyle)
+            edit_sh_fyle = 1
+            gencpy(srcdir,desdir,sh_md_fyle)
             
+    return continue_run, edit_sh_fyle
+#------------------------------------------------------------------
 
+# Create forcefield directory
+def create_ff_dir(destdir):
+    ff_dir = destdir + '/forcefields.ff'
+    if not os.path.isdir(ff_dir):
+        os.mkdir(ff_dir)
+    return ff_dir
+#------------------------------------------------------------------
 
+# Check and copy solvent topology/initguess/prm files
+def cpy_solv_files(top_dir,conf_dir,prm_dir,destdir,inp_type,\
+                   osoltype,wat_typ='tip3p'):
+    
+    top_fyl = []; prm_fyl = []
+    if inp_type == 'cosolvents':
 
+        # Check topology
+        src_dir = top_dir
+        ext = '.top'
+        o_file = osoltype + ext
+        if not os.path.exists(src_dir + '/' + o_file):
+            raise RuntimeError(o_file + "not found in " + src_dir)
+        gencpy(src_dir,ff_dir,o_file)
+        top_fyl.append(o_file)
 
+        w_file = wat_typ + ext
+        if not os.path.exists(src_dir + '/' + w_file):
+            raise RuntimeError(w_file + "not found in " + src_dir)
+        gencpy(src_dir,ff_dir,w_file)
+        top_fyl.append(w_file)
 
-#Main Code
+        # Check prm
+        src_dir = prm_dir
+        ext = '.prm'
+        o_file = osoltype + ext
+        if not os.path.exists(src_dir + '/' + o_file):
+            raise RuntimeError(o_file + "not found in " + src_dir)
+        gencpy(src_dir,ff_dir,o_file)
+        prm_fyl.append(o_file)
+
+        w_file = wat_typ + ext
+        if not os.path.exists(src_dir + '/' + w_file):
+            raise RuntimeError(w_file + "not found in " + src_dir)
+        gencpy(src_dir,ff_dir,w_file)
+        prm_fyl.append(w_file)
+
+        # Check gro/pdb conf file
+        src_dir = conf_dir
+        ext1 = '.gro'; ext2 = '.pdb'
+        o_file1 = osoltype + ext1; o_file2 = osoltype + ext2; 
+        if os.path.exists(src_dir + '/' + o_file1):
+            gencpy(src_dir,ff_dir,o_file1)
+        elif os.path.exists(src_dir + '/' + o_file2):
+            gencpy(src_dir,ff_dir,o_file2)
+        else:
+            raise RuntimeError(osoltype+"conf not found in "+src_dir)
+
+        src_dir = conf_dir
+        ext1 = '.gro'; ext2 = '.pdb'
+        w_file1 = wat_typ + ext1; w_file2 = wat_typ + ext2; 
+        if os.path.exists(src_dir + '/' + w_file1):
+            gencpy(src_dir,ff_dir,w_file1)
+        elif os.path.exists(src_dir + '/' + w_file2):
+            gencpy(src_dir,ff_dir,w_file2)
+            w_conf_fyl = '.pdb'
+        else:
+            raise RuntimeError(wat_typ+"conf not found in "+src_dir)
+
+    return top_fyl,prm_fyl
+
+def edit_main_top_file(main_topfyle,ff_dir,top_fyl,prm_fyl):
+    
