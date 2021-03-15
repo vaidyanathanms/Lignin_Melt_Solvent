4e5425acf868ed36d422b2bd80873a7384c7cf0c
vaidyanathanms <msvaidyanathan1729@gmail.com>
2021-03-15 01:10:24 -0400

    now the code creates preprocessing and md files for cosolvents/melts and solvent systems

--- files_ligmelt_solv/myruns/src_py/input_gen.py
+++ files_ligmelt_solv/myruns/src_py/input_gen.py
@@ -31,24 +31,28 @@ else:
 inp_type  = 'cosolvents'       # melts, solvents, cosolvents
 biomass   = 'WT' # name of the biomass type
 disperse  = 'mono' # mono/poly; only for melts
-o_sol_typ = 'EOH'  # relevant only for solvents/cosolvents
-wat_type  = 'tip3p' # relevant only for coslvents
-num_cases = 3      # for solvents, this will be == len(norg_solv)
+o_sol_typ = 'EOH'  # prefix for solvent file for solvents/cosolvents
+wat_type  = 'tip3p' # prefix for water file for coslvents
+solv_name = o_sol_typ # change if prefix is different from name in PDB
+wat_name  = 'TIP3_' # diff from prefix
+run_arr   = [1] # run number
 nchains   = 1     # number of polymer chains
-norg_solv = 1000 # number of organic solvents
+npoly_res = 22  # number of polymer residues
+n_orgsolv = 1000 # number of organic solvents
 nwater    = 4000 # number of water molecules (for cosolvents)
 box_dim   = 15 # box size for solvent only. cosolvent=+3
 #------------------------------------------------------------------
 
 # Directory Paths
-main_dir  = os.getcwd()
-gmx_dir   = '../src_gmx'
-top_dir   = gmx_dir + '/solv_files/topol'
-conf_dir  = gmx_dir + '/solv_files/initguess'
-prm_dir   = gmx_dir + '/solv_files/prm_itp'
-mdp_dir   = gmx_dir + '/' + 'mdp_files'
-sh_dir    = gmx_dir + '/' + 'sh_files'
-scr_dir   = '/gpfs/alpine/bip189/scratch/vaidyams/'
+main_dir  = os.getcwd() # current dir
+gmx_dir   = '../src_gmx' # gmx file super directory
+top_dir   = gmx_dir + '/solv_files/topol' # topology dir
+cfg_dir   = gmx_dir + '/solv_files/initguess' # configuration dir
+prm_dir   = gmx_dir + '/solv_files/prm_itp' # prm file dir
+mdp_dir   = gmx_dir + '/' + 'mdp_files' # mdp file dir
+sh_dir    = gmx_dir + '/' + 'sh_files' # sh file dir
+scr_dir   = '/gpfs/alpine/bip189/scratch/vaidyams' # scratch dir
+
 if not os.path.isdir(scr_dir):
     print("FATAL ERROR: ", scr_dir, " not found")
     exit("Check scratch directory path")
@@ -58,34 +62,41 @@ if not os.path.isdir(scr_dir):
 #------------------------------------------------------------------
 
 # Required GMX/sh Files
-mdp_fyles  = ['minim_pyinp.mdp','nvt_pyino.mdp',\
+mdp_fyles  = ['minim_pyinp.mdp','nvt_pyinp.mdp',\
               'npt_berendsen_pyinp.mdp','npt_main_pyinp.mdp']
-sh_md_fyle = 'run_md_pyinp.inp'
+sh_md_fyle = 'run_md_pyinp.sh'
 sh_pp_fyle = 'run_preprocess_pyinp.sh'
 sh_rep_fyl = 'repeat.sh'
 #------------------------------------------------------------------
 
 #Main Code
-for casenum in range(num_cases):
+for casenum in range(len(run_arr)):
     
-    print( "Run number: ", casenum + 1)
+    print( "Run number: ", run_arr[casenum])
       
     # Make directories
     head_dir = scr_dir + '/' + inp_type
     if not os.path.isdir(head_dir):
-        os.mkdir(head_dir)
+        print(head_dir, " does not exist")
+        print("ERR: Create path")
+        continue
 
     poly_dir = head_dir + '/' + biomass
     if not os.path.isdir(poly_dir):
-        os.mkdir(poly_dir)
+        print(poly_dir, " does not exist")
+        print("ERR: Create path")
+        continue
 
     if inp_type == 'melts':
         poly_dir = poly_dir + '/' + disperse
         if not os.path.isdir(poly_dir):
-            os.mkdir(poly_dir)
+            print(poly_dir, " does not exist")
+            print("ERR: Create path")
+            continue
     
-    rundir = poly_dir + '/run_' + str(casenum+1)
+    rundir = poly_dir + '/run_' + str(run_arr[casenum])
     if not os.path.isdir(rundir):
+        print(rundir, " does not exist")
         print('ERR: Create path and input top/pdb files first')
         continue
 
@@ -96,47 +107,52 @@ for casenum in range(num_cases):
         ref_temp,ref_pres,melt_topname = couple_coeff(inp_type,\
                                                       coeff_fyle)
 
+    # Check for pdb/psf/top files of the melt/polymer
+    poly_conffile,poly_topfile=check_inp_files(workdir1,melt_topname)
+
+    # Find tc_groups
+    tc_grp,tc_typ=create_tcgrps(inp_type,npoly_res,solv_name,wat_name)
+    
     # Copy and edit mdp files
-    check_cpy_mdp_files(mdp_dir,workdir1,mdp_fyles,Tetau_nvt,\
-                        Tetau_berend,Tetau_parrah,Prtau_berend,\
-                        Prtau_parrah,ref_temp,ref_pres,tc_grps,tc_type)
+    check_cpy_mdp_files(mdp_dir,workdir1,mdp_fyles,inp_type,Tetau_nvt\
+                        ,Tetau_berend,Tetau_parrah,Prtau_berend,\
+                        Prtau_parrah,ref_temp,ref_pres,tc_grp,tc_typ,\
+                        main_dir,coeff_fyle)
     cont_run,edit_sh_fyle = cpy_sh_files(sh_dir,workdir1,\
                                          sh_pp_fyle,sh_md_fyle)
 
-    # Check for pdb/psf/top files of the melt/polymer
-    poly_conffile,poly_topfile = check_inp_files(workdir1,melt_topname)
-
-    # Change to working directory
-    os.chdir(workdir1)
 
     # Copy/edit top/conf/prm files for solvents/cosolvents
     ff_dir = create_ff_dir(workdir1)
     if inp_type == 'solvents' or inp_type == 'cosolvents':
-        if cont_run = 0:
+        if cont_run == 0:
             # sol_top/sol_prm/sol_cfg are arrays
-            sol_top,sol_prm,sol_cfg=cpy_solv_files(top_dir,gro_dir,\
+            sol_top,sol_prm,sol_cfg=cpy_solv_files(top_dir,cfg_dir,\
                                                    prm_dir,ff_dir,\
                                                    inp_type,o_sol_typ,\
-                                                   wat_typ)
+                                                   wat_type)
             
-            edit_main_top_file(poly_topfile,ff_dir,sol_top,sol_prm)
+            poly_topedit = edit_main_top_file(poly_topfile,ff_dir,\
+                                              sol_top,sol_prm,workdir1)
+
+    # Copy and edit repeat.sh first time
+    if not os.path.exists(workdir1 + '/' + sh_rep_fyl):
+        gencpy(sh_dir,workdir1,sh_rep_fyl)
+
+    # Change to working directory
+    os.chdir(workdir1)
 
     # Edit shell script files if needed (edit_sh_fyle = 1)
     if edit_sh_fyle:
         if inp_type == 'melts':
-            edit_sh_files(workdir,cont_run,biomass,inp_type,poly_conffile\
-                          ,0,0,poly_topfile,'None','None',sh_pp_fyle,\
-                          sh_md_fyle,ff_dir,'None',0)
+            edit_sh_files(workdir1,cont_run,biomass,inp_type,poly_conffile\
+                          ,0,0,poly_topedit,'None','None',sh_pp_fyle\
+                          ,sh_md_fyle,ff_dir,'None',0)
         elif inp_type == 'solvents':
-            edit_sh_files(workdir,cont_run,biomass,inp_type,poly_conffile\
-                          ,n_org_solv,0,poly_topfile,o_sol_typ,'None',\
+            edit_sh_files(workdir1,cont_run,biomass,inp_type,poly_conffile,\
+                          n_orgsolv,0,poly_topedit,o_sol_typ,'None',\
                           sh_pp_fyle,sh_md_fyle,ff_dir,sol_cfg,box_dim)
         elif inp_type == 'cosolvents':
-            edit_sh_files(workdir,cont_run,biomass,inp_type,poly_conffile\
-                          ,n_org_solv,nwater,poly_topfile,o_sol_typ,wat_type\
-                          ,sh_pp_fyle,sh_md_fyle,ff_dir,sol_cfg,box_dim)
-            
-    
-    # Copy and edit repeat.sh first time
-    if not os.path.exists(workdir + '/' + sh_rep_fyle)
-       gencpy(shdir,workdir1,sh_rep_fyle
+            edit_sh_files(workdir1,cont_run,biomass,inp_type,poly_conffile,\
+                          n_orgsolv,nwater,poly_topedit,o_sol_typ,wat_type,\
+                          sh_pp_fyle,sh_md_fyle,ff_dir,sol_cfg,box_dim)
--- files_ligmelt_solv/myruns/src_py/make_gmx_inputs.py
+++ files_ligmelt_solv/myruns/src_py/make_gmx_inputs.py
@@ -33,7 +33,7 @@ def set_working_dir(rundir,inp_type,solv_type = 'None'):
     elif inp_type == 'cosolvents':
         h_workdir = rundir + '/' + solv_type
         workdir1 = h_workdir + '/' + solv_type + '_water'
-    elif:
+    else:
         workdir1 = rundir #even for inp_type = melts
 
     if not os.path.isdir(workdir1):
@@ -63,7 +63,7 @@ def couple_coeff(inp_type,coeff_fyle = 'None'):
                 if line.startswith('#'):
                     continue
                 words = line.split()
-                if words[0] = 'TauTemp_NVT':
+                if words[0] == 'TauTemp_NVT':
                     tau_temp_nvt  = float(words[1])
                 elif words[0] == 'TauTemp_Berendsen':
                     tau_temp_berend = float(words[1])
@@ -87,38 +87,113 @@ def couple_coeff(inp_type,coeff_fyle = 'None'):
         melt_topname
 #------------------------------------------------------------------
 
+#Check pdb/psf/top files for the melt (or polymer)
+def check_inp_files(dum_inpdir,top_name):
+    # check structure files (*.pdb/.gro)
+    if glob.glob(dum_inpdir+'/*.pdb') == [] and \
+       glob.glob(dum_inpdir+'/*.gro') == []:
+        raise RuntimeError("No polymer pdb/gro files found")
+    elif len(glob.glob(dum_inpdir+'/*.gro')) == 1:
+        conf_fname = glob.glob(dum_inpdir+'/*.gro')[0]
+    elif len(glob.glob(dum_inpdir+'/*.pdb')) == 1:
+        conf_fname = glob.glob(dum_inpdir+'/*.pdb')[0]
+    elif len(glob.glob(dum_inpdir+'/*.gro')) > 1:
+        print('More than one config file found. Using latest')
+        fnames = glob.glob(dum_inpdir+'/*.gro')
+        conf_fname = max(fnames, key=os.path.getctime)
+
+    # check topology files
+    if glob.glob(dum_inpdir+'/*.top') == []:
+        raise RuntimeError("No polymer topology files found")
+    if top_name != 'None':
+        if not os.path.exists(dum_inpdir + '/' + top_name):
+            raise RuntimeError("Specified poly top file not found")
+        else:
+            topol_fname = top_name
+    elif len(glob.glob(dum_inpdir+'/*.top')) == 1 and \
+         top_name == 'None':
+        topol_fname = glob.glob(dum_inpdir+'/*.top')[0]
+    else:
+        print('More than one topology file found. Using latest')
+        fnames = glob.glob(dum_inpdir+'/*.top')
+        topol_fname = max(fnames, key=os.path.getctime)
+    
+    return conf_fname, topol_fname
+
+#------------------------------------------------------------------
+
+# Create temperature coupling groups
+def create_tcgrps(inp_type,npoly_res,solv_name,wat_name):
+    if inp_type == 'melts':
+        tc_grp = 'System'
+        tc_typ = 'Single'
+    elif inp_type == 'solvents':
+        tc_grp = 'resnr_1_to_'+ str(npoly_res)
+        tc_grp = tc_grp + '\t' + 'resname_' + solv_name
+        tc_typ = 'Multi '
+    elif inp_type == 'cosolvents':
+        tc_grp = 'resnr_1_to_'+ str(npoly_res)
+        tc_grp = tc_grp + '\t' + 'resname_' + solv_name
+        tc_grp = tc_grp + '\t' + 'resname_' + wat_name
+        tc_typ = 'Multi '
+    return tc_grp, tc_typ
+#------------------------------------------------------------------
+
 # Check for mdp files and copy/edit if not present
-def check_cpy_mdp_files(srcdir,destdir,mdp_fyles,Tetau_nvt,\
-                        Tetau_berend,Tetau_parrah,Prtau_berend,\
+def check_cpy_mdp_files(srcdir,destdir,mdp_fyles,inp_type,Tetau_nvt\
+                        ,Tetau_berend,Tetau_parrah,Prtau_berend,\
                         Prtau_parrah,ref_temp,ref_pres,tc_grpdata,\
-                        tc_grptype,coeff_fyle='None'):
+                        tc_grptype,headdir,coeff_fyle='None'):
 
-    if coeff_fyle != 'None':
-        gencpy(srcdir,destdir,coeff_fyle)
+    if coeff_fyle != 'None': #gmx inp file from sys.argv
+        print("copying", coeff_fyle)
+        gencpy(headdir,destdir,coeff_fyle)
 
-    if not os.path.exists(mdp_fyles[fylcnt]):
-        gencpy(srcdir,destdir,mdp_fyles[fylcnt])
-        py_fname = mdp_fyles[fylcnt]
-        rev_fname = py_fname.replace('_pyinp','')
-        print(rev_fname)
-        fr  = open(py_fname,'r')
-        fw  = open(rev_fname,'w')
+    str_Tetau_nvt = genstr(inp_type,Tetau_nvt)
+    str_Tetau_ber = genstr(inp_type,Tetau_berend)
+    str_Tetau_par = genstr(inp_type,Tetau_parrah)
+    str_Prtau_ber = genstr(inp_type,Prtau_berend)
+    str_Prtau_par = genstr(inp_type,Prtau_parrah)
+    str_temp = genstr(inp_type,ref_temp)
+    str_pres = genstr(inp_type,ref_pres)
+
+    for mdp_fname in mdp_fyles:
+        if not os.path.exists(srcdir + '/' + mdp_fname):
+            raise RuntimeError(mdp_fname," not found in ",srcdir)
+        gencpy(srcdir,destdir,mdp_fname)
+        py_fname = mdp_fname
+        rev_fname = mdp_fname.replace('_pyinp','')
+        fr  = open(destdir + '/' + py_fname,'r')
+        fw  = open(destdir + '/' + rev_fname,'w')
         fid = fr.read().replace("py_tcgrps",tc_grpdata).\
               replace("py_grptype",tc_grptype).\
-              replace("py_Temptau_vr",str(Tetau_nvt)).\
-              replace("py_Temptau_Berend", str(Tetau_berend)).\
-              replace("py_Temptau_ParRah",str(Tetau_parrah)).\
-              replace("py_Prestau_Berend",str(Prtau_berend)).\
-              replace("py_Prestau_ParRah", str(Prtau_parrah)).\
-              replace("py_ref_t",str(ref_temp)).\
-              replace("py_ref_p",str(ref_pres))
+              replace("py_Temptau_vr",str_Tetau_nvt).\
+              replace("py_Temptau_Berend", str_Tetau_ber).\
+              replace("py_Temptau_ParRah",str_Tetau_par).\
+              replace("py_Prestau_Berend",str_Prtau_ber).\
+              replace("py_Prestau_ParRah", str_Prtau_par).\
+              replace("py_ref_t",str_temp).\
+              replace("py_ref_p",str_pres)
         fw.write(fid)
         fw.close()
         fr.close()
 #------------------------------------------------------------------
 
+# Generate strings
+def genstr(inp_type,inp_vals):
+    if inp_type == 'melts':
+        out_str = str(inp_vals)
+    elif inp_type == 'solvents':
+        out_str = str(inp_vals) + '\t' + str(inp_vals)
+    elif inp_type == 'cosolvents':
+        out_str = str(inp_vals) + '\t' + str(inp_vals) + '\t' + \
+                  str(inp_vals)
+    return out_str
+#------------------------------------------------------------------
+
 # Copy shell script files
 def cpy_sh_files(srcdir,destdir,sh_pp_fyle,sh_md_fyle):
+
     edit_sh_fyle = 0
     # Check for tpr files to provide run conditions
     if glob.glob(destdir+'/*.tpr') == []:
@@ -129,67 +204,27 @@ def cpy_sh_files(srcdir,destdir,sh_pp_fyle,sh_md_fyle):
         print('tpr files found. Continuing runs..')
         continue_run = 1
 
-    if rerun == 0: 
+    if continue_run == 0: 
         if not os.path.exists(srcdir+'/' + sh_pp_fyle):
-            raise RuntimeError(sh_pp_fyle + "not found in" +\
+            raise RuntimeError(sh_pp_fyle+" not found in "+\
                                srcdir)
-        gencpy(srcdir,desdir,sh_pp_fyle)
+        gencpy(srcdir,destdir,sh_pp_fyle)
 
         if not os.path.exists(srcdir+'/' + sh_md_fyle):
-            raise RuntimeError(sh_md_fyle + "not found in" +\
+            raise RuntimeError(sh_md_fyle+" not found in "+\
                                srcdir)
-        gencpy(srcdir,desdir,sh_md_fyle)
+        gencpy(srcdir,destdir,sh_md_fyle)
         
 
-    if rerun == 1: #at least one tpr file present
+    if continue_run == 1: #at least one tpr file present
         sh_mdrun = sh_md_fyle.replace('_pyinp','')
         if not os.path.exists(destdir+'/' + sh_mdrun):
-            print("WARNING: ", sh_mdrun, "not found in", destdir)
+            print("WARNING: ", sh_mdrun," not found in ",destdir)
             print("Recopying ", sh_md_fyle)
             edit_sh_fyle = 1
-            gencpy(srcdir,desdir,sh_md_fyle)
+            gencpy(srcdir,destdir,sh_md_fyle)
             
     return continue_run, edit_sh_fyle
-#------------------------------------------------------------------
-
-#Check pdb/psf/top files for the melt (or polymer)
-def check_inp_files(dum_inpdir,top_name):
-    # check structure files (.pdb/.gro)
-    if glob.glob(dum_inpdir+'/*.pdb') == [] and \
-       glob.glob(dum_inpdir+'/*.gro') == []:
-        raise RuntimeError("No polymer pdb/gro files found")
-    elif len(glob.glob(dum_inpdir+'/*.gro')) == 1:
-        conf_name = glob.glob(dum_inpdir+'/*.gro')
-    elif len(glob.glob(dum_inpdir+'/*.pdb')) == 1:
-        conf_name = glob.glob(dum_inpdir+'/*.pdb')
-    elif len(glob.glob(dum_inpdir+'/*.gro')) > 1:
-        print('More than one config file found. Using latest')
-        fnames = glob.glob(dum_inpdir+'/*.gro')
-        conf_fname = max(fnames, key=os.path.getctime)
-    elif len(glob.glob(dum_inpdir+'/*.gro')) > 1:
-        print('More than one config file found. Using latest')
-        fnames = glob.glob(dum_inpdir+'/*.pdb')
-        conf_fname = max(fnames, key=os.path.getctime)
-        
-
-    # check topology files
-    if glob.glob(dum_inpdir+'/*.top') == []:
-        raise RuntimeError("No polymer topology files found")
-    if top_name != 'None':
-        if not os.path.exists(dum_inpdir + '/' + top_name):
-            raise RuntimeError("Specified poly top file not found")
-        else:
-            topol_name = top_name
-    elif len(glob.glob(dum_inpdir+'/*.top')) == 1 and \
-         top_name == 'None':
-        topol_name = glob.glob(dum_inpdir+'/*.top')
-    else:
-        print('More than one topology file found. Using latest')
-        fnames = glob.glob(dum_inpdir+'/*.top')
-        topol_fname = max(fnames, key=os.path.getctime)
-    
-    return conf_fname, topol_fname
-
 #------------------------------------------------------------------
 
 # Create forcefield directory
@@ -201,7 +236,7 @@ def create_ff_dir(destdir):
 #------------------------------------------------------------------
 
 # Check and copy solvent topology/initguess/prm files
-def cpy_solv_files(top_dir,conf_dir,prm_dir,destdir,inp_type,\
+def cpy_solv_files(top_dir,conf_dir,prm_dir,ff_dir,inp_type,\
                    osoltype,wat_typ='tip3p'):
     
     top_fyl = []; prm_fyl = []; conf_fyl = [];
@@ -210,14 +245,14 @@ def cpy_solv_files(top_dir,conf_dir,prm_dir,destdir,inp_type,\
         # Check topology
         src_dir = top_dir; o_file = osoltype + '.top'
         if not os.path.exists(src_dir + '/' + o_file):
-            raise RuntimeError(o_file + "not found in " + src_dir)
+            raise RuntimeError(o_file + " not found in "+src_dir)
         gencpy(src_dir,ff_dir,o_file)
         top_fyl.append(o_file)
 
         # Check prm
         src_dir = prm_dir; o_file = osoltype + '.prm'
         if not os.path.exists(src_dir + '/' + o_file):
-            raise RuntimeError(o_file + "not found in " + src_dir)
+            raise RuntimeError(o_file+" not found in " + src_dir)
         gencpy(src_dir,ff_dir,o_file)
         prm_fyl.append(o_file)
 
@@ -231,7 +266,7 @@ def cpy_solv_files(top_dir,conf_dir,prm_dir,destdir,inp_type,\
             gencpy(src_dir,ff_dir,o_file2)
             conf_fyl.append(o_file2)
         else:
-            raise RuntimeError(osoltype+"conf not found in "+src_dir)
+            raise RuntimeError(osoltype+" conf not found in "+src_dir)
 
     if inp_type == 'cosolvents': #with second solvent; def: water
         
@@ -257,49 +292,89 @@ def cpy_solv_files(top_dir,conf_dir,prm_dir,destdir,inp_type,\
             gencpy(src_dir,ff_dir,w_file2)
             conf_fyl.append(w_file2)
         else:
-            raise RuntimeError(wat_typ+"conf not found in "+src_dir)
+            raise RuntimeError(wat_typ+" conf not found in "+src_dir)
 
     return top_fyl,prm_fyl,conf_fyl
 #------------------------------------------------------------------
 
 # Edit melt/polymer topology to add solvent/cosolvent details
-def edit_main_top_file(workdir,main_topfyle,ff_dir,top_arr,prm_arr):
+def edit_main_top_file(main_topfyle,ff,top_arr,prm_arr,workdir):
+
+    # check whether main_topfyle has the required data. If so no need
+    # to edit and make the new ones. Slightly inefficient - but OK!
+    req_flag = 0 # default not present
+    for i in range(len(top_arr)):
+        with open(main_topfyle) as fchk:
+            for line in fchk:
+                if top_arr[i] in line:
+                    req_flag += 1 # found string
+
+    for i in range(len(prm_arr)):
+        with open(main_topfyle) as fchk:
+            for line in fchk:
+                if prm_arr[i] in line:
+                    req_flag += 1 # found string
+
+    sum_req = len(prm_arr) + len(top_arr)
+    if req_flag == sum_req:
+        print('Present topol file in the directory has all reqd data')
+        return main_topfyle
+    else:
+        print('Adding reqd data to new topology file')
+
+    # split strings for ease of reading in final file o/p
+    ff_dir_spl = ff.split("/")
+    ff_dir = "./" + ff_dir_spl[len(ff_dir_spl)-1]
+
+    spl_str = main_topfyle.split("/")
+    edited_file = workdir + "/solvated_" + spl_str[len(spl_str)-1]
+
+    # create new topology file with required details
     # add topology before [ moleculetype ]
     inc_top = ''
     inc_pre = '#include '
-    for i in len(range(top_arr)):
+    for i in range(len(top_arr)):
         inc_top = inc_top + inc_pre + "'" + ff_dir + '/' + \
-                  top_fyl[i] + "' " + '/n'
-
-    with open(workdir+'/'+main_topfyle) as fyl:
-        for line in fyl.readlines():
-            if "[ moleculetype ]" in line:
-                line=line.replace(line,inc_top+line)
+                  top_arr[i] + "' " + '\n'
+    inc_top = inc_top + '\n'
 
     # add parameters before [ system ]
     inc_prm = ''
     inc_pre = '#include '
-    for i in len(range(prm_arr)):
-        inc_prm = inc_top + inc_pre + "'" + ff_dir + '/' + \
-                  top_fyl[i] + "' " + '/n'
+    for i in range(len(prm_arr)):
+        inc_prm = inc_prm + inc_pre + "'" + ff_dir + '/' + \
+                  prm_arr[i] + "' " + '\n'
+    inc_prm = inc_prm + '\n'
 
-    with open(workdir+'/'+main_topfyle) as fyl:
-        for line in fyl.readlines():
-            if "[ system ]" in line:
-                line=line.replace(line,inc_top+line)
+    with open(main_topfyle,"r") as fin:
+        with open(edited_file,"w") as fout:
+            for line in fin:
+                if "[ moleculetype ]" in line:
+                    line=line.replace(line,inc_prm+line)
+                elif "[ system ]" in line:
+                    line=line.replace(line,inc_top+line)
+                fout.write(line)
+
+
 #------------------------------------------------------------------
 
 # Edit shell script files
-def edit_sh_files(workdir,cont_run,biomass,inp_type,poly_cfg,\
-                  nsolv,nwater,topfyle,o_sol_type,wat_typ,pp_fyle,\
-                  md_fyle,ff_dir,sol_cfg,dim):
+def edit_sh_files(workdir,cont_run,biomass,inp_type,polycfg,\
+                  nsolv,nwater,topfyle,o_sol_type,wat_typ,pp_fyle\
+                  ,md_fyle,ffdir,solcfg,dim):
+
+    # use the last element (others correspond to full path)
+    # underscored variable corresponds to split file name
+    top_fyle = ret_file_str(topfyle)
+    poly_cfg = ret_file_str(polycfg)
+    ff_dir   = ret_file_str(ffdir)
 
     if cont_run == 0:
         # job/box name
         jname = 'pp_'+ biomass
         if inp_type == 'solvents' or inp_type == 'cosolvents':
             jname = jname + '_' + o_sol_type #py_jobname
-        box_conffyle = "boxedit_" + conf_fyle
+        box_conffyle = "boxedit_" + poly_cfg
         
         # solvate commands
         solv_js = 'jsrun -X 1 -n 1 -c 7 -a 1 -g 1 ' + \
@@ -310,24 +385,29 @@ def edit_sh_files(workdir,cont_run,biomass,inp_type,poly_cfg,\
             solv_str2 = '# no cosolvation'
             fin_conf  = poly_cfg
         elif inp_type == 'solvents':
-            sol_cfg1 = ff_dir + '/' + sol_cfg[0]
+            sol_cfg  = ret_file_str(solcfg[0])
+            sol_cfg1 = ff_dir + '/' + sol_cfg
             solv_str1 = solv_js + ' -cp ' + poly_cfg + ' -cs ' + \
-                        sol_cfg1 + ' -p ' + topfyle + ' -o ' + \
+                        sol_cfg1 + ' -p ' + top_fyle + ' -o ' + \
                         'solv_'+ poly_cfg + ' -maxsol ' + str(nsolv)\
                         + ' -box ' + str(dim) + ' ' + str(dim) + ' '\
                         + str(dim) + '\n'
             solv_str2 = '# no cosolvation'
             fin_conf  = 'solv_' + poly_cfg
         elif inp_type == 'cosolvents':
-            sol_cfg1 = ff_dir + '/' + sol_cfg[0]
-            sol_cfg2 = ff_dir + '/' + sol_cfg[1]
+            sol_cfg  = ret_file_str(solcfg[0])
+            sol_cfg1 = ff_dir + '/' + sol_cfg
+            sol_cfg  = ret_file_str(solcfg[1])
+            sol_cfg2 = ff_dir + '/' + sol_cfg
             solv_str1 = solv_js + ' -cp ' + poly_cfg + ' -cs ' + \
-                        sol_cfg1 + ' -p ' + topfyle + ' -o ' + \
+                        sol_cfg1 + ' -p ' + top_fyle + ' -o ' + \
                         'solv_'+ poly_cfg + ' -maxsol ' + str(nsolv)\
                         + ' -box ' + str(dim) + ' ' + str(dim) + ' '\
                         + str(dim) + '\n'
+
+            dim += 3 # arbitrary 3 nm
             solv_str2 = solv_js + ' -cp ' +'solv_' +poly_cfg +' -cs '\
-                        + sol_cfg2 + ' -p ' + topfyle + ' -o ' + \
+                        + sol_cfg2 + ' -p ' + top_fyle + ' -o ' + \
                         'cosolv_'+ poly_cfg +' -maxsol '+str(nwater)\
                         + ' -box ' + str(dim) + ' ' + str(dim) + ' '\
                         + str(dim) + '\n'
@@ -338,12 +418,14 @@ def edit_sh_files(workdir,cont_run,biomass,inp_type,poly_cfg,\
         rev_fname = py_fname.replace('_pyinp','')
         fr  = open(py_fname,'r')
         fw  = open(rev_fname,'w')
-        fid = fr.read().replace("py_meltconf",conf_fyle).\
+        fid = fr.read().replace("py_jobname",jname).\
+              replace("py_meltconf",poly_cfg).\
               replace("py_boxmeltconf",box_conffyle).\
               replace("py_solvate_1",solv_str1).\
               replace("py_solvate_2", solv_str2).\
-              replace("py_topol",topfyle).\
+              replace("py_topol",top_fyle).\
               replace("py_finconf",fin_conf)
+        fw.write(fid)
         fr.close(); fw.close()
 
     # since edit_sh_fyle is true, the run_md_file is recopied
@@ -352,14 +434,21 @@ def edit_sh_files(workdir,cont_run,biomass,inp_type,poly_cfg,\
     jname = 'md_'+ biomass
     if inp_type == 'solvents' or inp_type == 'cosolvents':
         jname = jname + '_' + o_sol_type #py_jobname
-        box_conffyle = "boxedit_" + conf_fyle
 
     # edit pp_fyle
-    py_fname = pp_fyle
+    py_fname = md_fyle
     rev_fname = py_fname.replace('_pyinp','')
     fr  = open(py_fname,'r')
     fw  = open(rev_fname,'w')
-    fid = fr.read().replace("py_topol",topfyle).\
+    fid = fr.read().replace("py_jobname",jname).\
+          replace("py_topol",top_fyle).\
           replace("py_finconf",fin_conf)
+    fw.write(fid)
     fr.close(); fw.close()
+#------------------------------------------------------------------
+
+# returns the last element file string after stripping "/"
+def ret_file_str(inpstr):
+    spl_str = inpstr.split("/")
+    return spl_str[len(spl_str)-1]
 #------------------------------------------------------------------
